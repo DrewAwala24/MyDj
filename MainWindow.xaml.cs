847d7c63f15bc399dde4603654fd9044af50ba22
DrewAwala24 <andrewawala141@gmail.com>
2026-05-13 11:42:35 +0300

    Updated music player with GUI logic

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MyPersonalDj;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyPersonalDjGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        songmenu myMenu = new songmenu();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SongListBox.ItemsSource = (System.Collections.IEnumerable)myMenu.getLibrary();
+        }
+
+    }
+
+}
